fa3eca3b5afc7948c3c1c9ada72f19bfc91f3ac0
vinhhoang-1312 <tranphanvinhhoang7@gmail.com>
2021-08-08 14:39:48 +0700

    Use TouchableOpacity for button

--- App.js
+++ App.js
@@ -1,47 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 
-import { Button, SafeAreaViewBase,TextInput,Alert,ScrollView,Image,SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  SafeAreaViewBase,
+  TextInput,
+  Alert,
+  ScrollView,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = () => {
+    setSubmitted(!submitted);
+    if (submitted) {
+      setName("");
+      setAddress("");
+    }
+  };
   return (
-   <SafeAreaView style={styles.container}>
-    {/* //text// */}
-     <Text >Hello World !</Text>
-     <Button 
-     title= "Click me" />
-    <TextInput
+    <SafeAreaView style={styles.container}>
+      <Text>Hello World !</Text>
+
+      <TextInput
+        value={name}
         style={styles.input}
         onChangeText={(value) => setName(value)}
         placeholder="Enter your name ..."
       />
-        <TextInput
+      <TextInput
+        value={address}
         style={styles.input}
         onChangeText={(value) => setAddress(value)}
         placeholder="Enter your address ..."
       />
-      <Text style={styles.text}>Your name is: {name}</Text>
-      <Text style={styles.address}>Your address is: {address}</Text>
-      
-   </SafeAreaView>
+      <TouchableOpacity style={styles.Button} onPress={handleSubmit}>
+        <Text style={styles.text}>{submitted ? "Clear" : "Submit"}</Text>
+      </TouchableOpacity>
+
+      {submitted ? (
+        <View>
+          <Text style={styles.text}>Your name is: {name}</Text>
+          <Text style={styles.address}>Your address is: {address}</Text>
+        </View>
+      ) : (
+        <Text>Nothing to show</Text>
+      )}
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   container: {
-flex:1,
-    backgroundColor: '#fff',
-    alignItems:'center',
-    justifyContent:'center',
-  
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
- 
+
   input: {
     height: 40,
     width: "90%",
     margin: 12,
     borderWidth: 1,
     padding: 10,
+  },
+  Button: {
+    backgroundColor: "#00ff00",
+    width: 150,
+    height: 50,
+    alignItems: "center",
+    borderRadius: 10,
+    justifyContent: "center",
   },
 });
